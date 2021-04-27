1294acab18da3fbe1bc209010fd5d99d369dfe5e
Gael Sanchez <sgael95@gmail.com>
2021-04-27 15:40:25 -0500

    Initializing Repo

---
+++ src/test/java/com/contractordata/ContractorDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contractordata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
