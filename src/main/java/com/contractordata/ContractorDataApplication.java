79f8c120ba1a669f48a76cda0e84e33fd4a649b4
sgael95 <sgael95@gmail.com>
2021-05-07 19:39:11 -0500

    Fix bug in POST method for Pay service.

--- src/main/java/com/contractordata/ContractorDataApplication.java
+++ src/main/java/com/contractordata/ContractorDataApplication.java
@@ -2,12 +2,27 @@ package com.contractordata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ContractorDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContractorDataApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                    .allowedOrigins("http://localhost:3000")
+	                    .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+	        }
+	    };
 	}
 
 }
